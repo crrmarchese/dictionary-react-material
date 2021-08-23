96ab30d611826c60bc0e215f501709630e83e698
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-08-22 20:00:08 -0400

    Add mapping for meanings

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import './definitions.css';
 
-const Definitions = ({ searchWord, language, definitions }) => {
+// Props from App (Search Word, Language and Meaning--data from API)
+const Definitions = ({ searchWord, language, meanings }) => {
     return (
         <div className="word-definitions">
             {
                 searchWord === "" ? (<span className="subtitle">Start by typing a word in Search</span>) : (
-                    "Something"
+                    // Map through each item in data array > meanings array
+                    meanings.map((meaning) => 
+                        //console.log(meaning);
+                        // map through each array item in the meanings array
+                         meaning.meanings.map((item) => (
+                            //console.log(item);
+                            item.definitions.map((def) => (
+                                <div className="word-definition" style={{ backgroundColor: "white", color: "black"}}>
+                                   {def.definition} 
+                                   <hr style={{ backgroundColor: "black", width: "100%"}} />
+                                   {def.example && (
+                                       <span>
+                                           <strong>Example: </strong>
+                                           {def.example}
+                                       </span>
+                                   )
+
+                                   }
+                                </div>
+                            )
+                        ))
+                    )
                 )
-            }
+            )}
         </div>
     )
 }
